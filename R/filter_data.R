307f2189c27fd3ee1e9e38600f767eb3d2171866
Jasmine Cabrera <jkscabrera2003@gmail.com>
2026-05-29 10:25:39 -0700

    Add functions and test cases.

---
+++ R/filter_data.R
@@ -0,0 +1,20 @@
+#' Filters Data
+#'
+#' @param data Dataset to filter.
+#'
+#' @returns Filtered and cleaned dataset.
+#'
+#' @export
+#'
+#' @examples
+#' filter_data()
+
+filter_data <- function(data){
+  # filter cases month
+
+  # filter cases year
+
+  # only output one dataset
+
+  # check to see if it works with summarize_year and plotVaccination
+}
--- R/load_data.R
+++ R/load_data.R
@@ -9,6 +9,7 @@
 #' @export
 #'
 #' @examples
+#' load_data()
 
 load_data <- function(){
   list(cases_month = readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-24/cases_month.csv'),
